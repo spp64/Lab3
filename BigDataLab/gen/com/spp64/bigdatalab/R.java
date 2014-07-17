052bfd4974a5f603e65923e83a316b82a15d069d
Praneeth <spp64@mail.umkc.edu>
2014-07-17 15:38:59 -0500

    New Code
    Output in TextView

--- BigDataLab/gen/com/spp64/bigdatalab/R.java
+++ BigDataLab/gen/com/spp64/bigdatalab/R.java
@@ -14,15 +14,14 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int Button1=0x7f060001;
-        public static final int Button2=0x7f060002;
-        public static final int Button3=0x7f060003;
-        public static final int Button4=0x7f060004;
-        public static final int textView=0x7f060000;
+        public static final int Button1=0x7f060000;
+        public static final int Button2=0x7f060001;
+        public static final int Button3=0x7f060002;
+        public static final int Button4=0x7f060003;
+        public static final int textView=0x7f060004;
     }
     public static final class layout {
-        public static final int activity_create=0x7f030000;
-        public static final int activity_main=0x7f030001;
+        public static final int activity_main=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
--- BigDataLab/src/com/spp64/bigdatalab/MainActivity.java
+++ BigDataLab/src/com/spp64/bigdatalab/MainActivity.java
@@ -1,20 +1,40 @@
 package com.spp64.bigdatalab;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+
 import android.app.Activity;
-import android.content.Intent;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-	String text = "";
+	
+	String HTML;
+	TextView text;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		text = (TextView) findViewById(R.id.textView);
+		text.setMovementMethod(new ScrollingMovementMethod());
+		text.setText("");
 
 		Button create = (Button) findViewById(R.id.Button1);
 		create.setOnClickListener(new View.OnClickListener() {
@@ -23,9 +43,11 @@ public class MainActivity extends Activity {
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				Log.i("clicked", "create");
-				Intent i = new Intent(MainActivity.this, CreateActivity.class);
+				/*Intent i = new Intent(MainActivity.this, CreateActivity.class);
 				i.putExtra("Action", "create");
-				startActivity(i);
+				startActivity(i);*/
+				new Task()
+				.execute("http://134.193.136.147:8080/HbaseWS/jaxrs/generic/hbaseCreate/NewTableData/GeoLocation:Date:Accelerometer");
 			}
 		});
 
@@ -36,9 +58,8 @@ public class MainActivity extends Activity {
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 
-				Intent i = new Intent(MainActivity.this, CreateActivity.class);
-				i.putExtra("Action", "insert");
-				startActivity(i);
+				new Task()
+				.execute("http://134.193.136.147:8080/HbaseWS/jaxrs/generic/hbaseCreate/Blah101/GeoLocation:Date:Accelerometer");
 			}
 		});
 
@@ -66,9 +87,8 @@ public class MainActivity extends Activity {
 				 * } }.execute();
 				 */
 
-				Intent i = new Intent(MainActivity.this, CreateActivity.class);
-				i.putExtra("Action", "retrieve");
-				startActivity(i);
+				new Task()
+				.execute("http://134.193.136.147:8080/HbaseWS/jaxrs/generic/hbaseRetrieveAll/Blah101");
 			}
 		});
 
@@ -78,11 +98,64 @@ public class MainActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 
-				Intent i = new Intent(MainActivity.this, CreateActivity.class);
-				i.putExtra("Action", "delete");
-				startActivity(i);
+				new Task()
+				.execute("http://134.193.136.147:8080/HbaseWS/jaxrs/generic/hbasedeletel/Blah101");
 			}
 		});
+
+	}
+	
+	private class Task extends AsyncTask<String, Void, String> {
+		private ProgressDialog dialog = new ProgressDialog(MainActivity.this);
+
+		@Override
+		protected String doInBackground(String... url) {
+			try {
+
+				HttpClient httpClient = new DefaultHttpClient();
+				HttpContext localContext = new BasicHttpContext();
+				HttpGet httpGet = new HttpGet(url[0]); // URL!
+
+				HttpResponse response = httpClient.execute(httpGet,
+						localContext);
+				String result = "";
+
+				BufferedReader reader = new BufferedReader(
+						new InputStreamReader(response.getEntity().getContent()));
+
+				String line = null;
+				while ((line = reader.readLine()) != null) {
+					result += line + "\n";
+					HTML = result;
+				}
+
+			} catch (ClientProtocolException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			// TODO Auto-generated method stub
+			return HTML;
+		}
+
+		@Override
+		protected void onPreExecute() {
+			dialog.setMessage("In Progress...");
+			dialog.show();
+
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			dialog.dismiss();
+			System.out.println("Respose : " + result);
+			Log.i("result", result);
+			text.setText(result);
+			
+		}
 
 	}
 }
